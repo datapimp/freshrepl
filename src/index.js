f45aaef98be6504958d969d73b6a793cf311b90c
Jonathan Soeder <jonathan.soeder@gmail.com>
2016-09-21 22:45:50 -0500

    Removing context vars

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import axios from 'axios'
 import colors from 'colors'
-import lodash from 'lodash'
 import CLI from './cli'
+import omit from 'lodash/omit'
+import mapKeys from 'lodash/mapKeys'
+import camelCase from 'lodash/camelCase'
 import { join } from 'path'
 import { create as createRepl } from './repl'
 
 const argv = require('minimist')(process.argv)
 
 const ARGV = {
-  ...(lodash.omit(argv, '_')),
-  ...lodash.mapKeys(argv , (v, k) => lodash.camelCase(k) ),
+  ...(omit(argv, '_')),
+  ...mapKeys(argv , (v, k) => camelCase(k) ),
 }
 
 export const cli = CLI
@@ -17,17 +18,10 @@ export const cli = CLI
 export const create = (options = {}, context = {}, ready) => {
   const replServer = createRepl({
     historyFile: ARGV.history || join(process.env.HOME, '.freshrepl'),
-    prompt: `${'FRESH'.bold}${':'.grey}${colors.rainbow('REPL').bold}${colors.grey('> ')}`,
+    prompt: `${'FRESH'.bold}${':'.grey}${colors.rainbow('REPL').bold}${colors.grey('>')} `,
     ...options,
   }, {
     ARGV,
-    lodash,
-    axios,
-    get: (...args) => axios.get(...args),
-    post: (...args) => axios.post(...args),
-    put: (...args) => axios.put(...args),
-    del: (...args) => axios.del(...args),
-    cli: CLI,
     ...context,
   }, ready)
 
